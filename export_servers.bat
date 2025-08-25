101dd6ce2779d7ba3c052e7824cd1cc09d2db228
Nathan Jones <166842942+Zesty-Fork@users.noreply.github.com>
2025-08-25 13:54:45 -0700

    Add files via upload
    yaml and batch files to quickly automate server deployment.

---
+++ export_servers.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set godot_path="C:\Users\NathanJones\Desktop\Projects\_complete\Recycle Bin - Shortcut\Godot_v4.4.1-stable_win64.exe\Godot_v4.4.1-stable_win64.exe"
+set mq_authentication_path="C:\Users\NathanJones\Desktop\Projects\_complete\Recycle Bin - Shortcut\Godot\MQAuthentication"
+set mq_gateway_path="C:\Users\NathanJones\Desktop\Projects\_complete\Recycle Bin - Shortcut\Godot\MQGateway"
+
+
+%godot_path% --path %mq_authentication_path% --export-release "MQAuthentication" "dist\MQAuthentication.x86_64"
+%godot_path% --path %mq_gateway_path% --export-release "MQGateway" "dist\MQGateway.x86_64"
+
+docker compose up --build
