ae7f4f133a5c3bf2885abf2e05648f128ca748cb
Nathan James Jones <nathanjjones95@gmail.com>
2025-08-25 14:06:51 -0700

    Changed files to work in different subdirectories
    This is a bit more jank than I thought it would be, but I now have the bat file searching one folder up for server projects. It works for what I need it to do.

--- export_servers.bat
+++ export_servers.bat
@@ -1,5 +1,7 @@
 @echo off
 
+cd ..
+
 set godot_path="C:\Users\NathanJones\Desktop\Projects\_complete\Recycle Bin - Shortcut\Godot_v4.4.1-stable_win64.exe\Godot_v4.4.1-stable_win64.exe"
 set mq_authentication_path="C:\Users\NathanJones\Desktop\Projects\_complete\Recycle Bin - Shortcut\Godot\MQAuthentication"
 set mq_gateway_path="C:\Users\NathanJones\Desktop\Projects\_complete\Recycle Bin - Shortcut\Godot\MQGateway"
@@ -7,5 +9,7 @@ set mq_gateway_path="C:\Users\NathanJones\Desktop\Projects\_complete\Recycle Bin
 
 %godot_path% --path %mq_authentication_path% --export-release "MQAuthentication" "dist\MQAuthentication.x86_64"
 %godot_path% --path %mq_gateway_path% --export-release "MQGateway" "dist\MQGateway.x86_64"
+
+cd MQUtils
 
 docker compose up --build
