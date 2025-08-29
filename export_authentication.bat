15dd9b9fde987b485c2193acbe867ca05951ad70
Nathan Jones <nathanjjones95@gmail.com>
2025-08-28 22:08:47 -0700

    .env added and updated tools

--- export_authentication.bat
+++ export_authentication.bat
@@ -5,7 +5,7 @@ cd ..
 set godot_path="C:\Users\NathanJones\Desktop\Projects\_complete\Recycle Bin - Shortcut\Godot_v4.4.1-stable_win64.exe\Godot_v4.4.1-stable_win64.exe"
 set mq_path="%cd%\MQAuthentication"
 
-%godot_path% --headless --path %mq_path% --export-release "MQAuthentication" "dist\MQAuthentication.x86_64"
+%godot_path% --headless --path %mq_path% --export-release "Linux" "dist\MQAuthentication.x86_64"
 
 cd MQUtils
 
